fe55b95d1e1fb2905883bab478cb10c1b10c574e
katp10 <kathylee1085@gmail.com>
2019-06-29 23:08:42 -0400

    while loops

---
+++ string/src/string/funWords.java
@@ -0,0 +1,16 @@
+package string;
+
+public class funWords {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int myInt= 0;
+		
+		while(myInt<10)
+		{
+			System.out.println(myInt + 1);
+			myInt = myInt + 1;
+		}
+	}
+
+}
