2665c90f86faca333b9cad51b489cc79764ba538
katp10 <kathylee1085@gmail.com>
2019-06-29 22:31:54 -0400

    variable practice

---
+++ vari/src/variable/app.java
@@ -0,0 +1,26 @@
+package variable;
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int myNumber = 88;
+short myShort = 345;
+long myLong = 59645;
+
+double myDouble = 5.4434;
+float myFloat = 4345.5f;
+char myChar = 'k';
+boolean myBoolean = false;
+byte myByte = 127;
+
+System.out.println(myNumber);
+System.out.println(myShort);
+System.out.println(myLong);
+System.out.println(myDouble);
+System.out.println(myFloat);
+System.out.println(myChar);
+System.out.println(myBoolean);
+System.out.println(myByte);
+	}
+
+}
---
+++ variables/src/variables/newvariable.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class newvariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
