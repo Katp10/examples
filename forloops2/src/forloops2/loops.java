5e34f7bdeb5f6a62e19bcde064808643d01745e8
katp10 <kathylee1085@gmail.com>
2019-07-03 21:27:32 -0400

    if statements

---
+++ forloops2/src/forloops2/loops.java
@@ -0,0 +1,9 @@
+package forloops2;
+
+public class loops {
+	public static void main(String[] args) {
+		for(int i= 25;i<10;i++) {
+	        System.out.printf("The value is:%d\n", i);
+		}
+	}
+}
---
+++ vari/src/variable/ifrules.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class ifrules {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int myNumber = 200;
+ if(myNumber<50) {
+	 System.out.println("Less Than 50");
+ }if(myNumber<200){
+	 System.out.println("Under 200");
+ }if(myNumber == 200) {
+	 System.out.println("You've got it!!");
+ }
+	}
+
+}
